b8b37fb42cfd5e0d32df21eecc17f9873d4d8581
baharmostafaei <120775848+baharmostafaei@users.noreply.github.com>
2023-09-17 09:14:42 +0330

    Preprocess the Data Create a Validation Set and Scale It

--- mnist.py
+++ mnist.py
@@ -2,3 +2,20 @@ import numpy as np
 import tensorflow as tf
 import tensoeflow_datasets as tfds
 
+mnist_dataset , mnist_info = tfds.load(name='mnist', with_info=True, as_supervised=True)
+
+mnist_train, mnist_test, mnist_dataset['trian'], mnist_dataset['test']
+
+num_validation_sample = 0.1 * mnist_info.splits['train'].num_example
+
+num_validation_sample = tf.cast(num_validation_sample, tf.int64)
+
+
+num_test_sample = mnist_info.splits['test'].num_examples
+num_test_sample = tf.cast(num_test_sample, tf.int64)
+
+def scale(image, lable):
+    image = tf.cast(image, tf.float32)
+    image /= 255.
+    return image, lable
+
